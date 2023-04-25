825447a772354789bcae482bf0e381aadd5fbd07
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-04-25 14:31:42 +0100

    manage places using state

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:flutter_favourite_places/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [...state, newPlace];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider(
+  (ref) => UserPlacesNotifier(),
+);
