190b2744bad7bab296234243d155fa84e1565aa1
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-05-09 11:14:13 +0100

    store image locally

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,19 +1,25 @@
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 import 'package:flutter_favourite_places/models/place.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(
+  Future<void> addPlace(
     String title,
     File image,
     PlaceLocation location,
-  ) {
+  ) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
     final newPlace = Place(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
     state = [...state, newPlace];
