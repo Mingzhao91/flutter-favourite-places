d815a5b7e532f47d013d8bdb00574f7f68096fa6
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2023-05-09 11:25:43 +0100

    store data in database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:flutter_favourite_places/models/place.dart';
 
@@ -22,6 +24,29 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       image: copiedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+
+    db.insert(
+      'user_places',
+      {
+        'id': newPlace.id,
+        'title': newPlace.title,
+        'image': newPlace.image.path,
+        'lat': newPlace.location.latitude,
+        'lng': newPlace.location.longitude,
+        'address': newPlace.location.address,
+      },
+    );
+
     state = [...state, newPlace];
   }
 }
